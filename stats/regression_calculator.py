a7280dcc83b7d2bf869bae147595fdea9c01c909
willbe <rlakim5521@naver.com>
2018-01-25 22:36:26 -0600

    1. Add a summary logic to get_ridge_regression function.

--- stats/regression_calculator.py
+++ stats/regression_calculator.py
@@ -4,6 +4,7 @@
 :Date: 2018. 1. 24.
 """
 import pandas as pd
+import numpy as np
 
 from sklearn.linear_model import Ridge, LogisticRegression
 
@@ -24,17 +25,30 @@ def get_logistic_regression(x_train, y_train, x_test):
     return pd.Series(y_prediction)
 
 
-def get_ridge_regression(x_train, y_train, x_test, alpha):
+def get_ridge_regression(x_train, y_train, x_test, alpha, summary=False):
     """
 
-    :param x_train:
-    :param y_train:
-    :param x_test:
-    :param alpha:
+    :param x_train: (DataFrame) The variables of train set.
+    :param y_train: (Series) The correct answers of train set.
+    :param x_test: (DataFrame) The variables of test set.
+    :param alpha: Regularization strength; must be a positive float. Regularization
+        improves the conditioning of the problem and reduces the variance of
+        the estimates. Larger values specify stronger regularization.
+        Alpha corresponds to ``C^-1`` in other linear models such as
+        LogisticRegression or LinearSVC. If an array is passed, penalties are
+        assumed to be specific to the targets. Hence they must correspond in
+        number.
+    :param summary: (bool) If summary is True, print the coefficient values by descent order.
     :return:
     """
     model = Ridge(alpha=alpha)
     model.fit(x_train, y_train)
+
+    if summary:
+        model_coef = pd.DataFrame(data=list(zip(x_train.columns, np.abs(model.coef_))),
+                                  columns=['column_name', 'coefficient_value'])
+        model_coef = model_coef.sort_values(by='coefficient_value', ascending=False)
+        print(model_coef)
 
     y_prediction = model.predict(X=x_test)
 
