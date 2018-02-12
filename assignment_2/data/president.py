a9130028e41c37a386954cae13d8c22becc09aa7
willbe <rlakim5521@naver.com>
2018-02-11 23:20:46 -0600

    1. Add speeches of presidents.

---
+++ assignment_2/data/president.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+:Author: Jaekyoung Kim
+:Date: 2018. 2. 9.
+"""
+Washington = 'George Washington'
+Adams = 'John Adams'
+Jefferson = 'Thomas Jefferson'
+Madison = 'James Madison'
+Monroe = 'James Monroe'
+Quincy_Adams = 'John Quincy Adams'
+Jackson = 'Andrew Jackson'
+Buren = 'Martin Van Buren'
+William_Harrison = 'William Henry Harrison'
+Tyler = 'John Tyler'
+Polk = 'James K. Polk'
+Taylor = 'Zachary Taylor'
+Fillmore = 'Millard Fillmore'
+Pierce = 'Franklin Pierce'
+Buchanan = 'James Buchanan'
+Lincoln = 'Abraham Lincoln'
+Andrew_Johnson = 'Andrew Johnson'
+Grant = 'Ulysses S. Grant'
+Hayes = 'Rutherford B. Hayes'
+Garfield = 'James A. Garfield'
+Arthur = 'Chester A. Arthur'
+Cleveland = 'Grover Cleveland'
+Benjamin_Harrison = 'Benjamin Harrison'
+McKinley = 'William McKinley'
+Theodore_Roosevelt = 'Theodore Roosevelt'
+Taft = 'William H. Taft'
+Wilson = 'Woodrow Wilson'
+Harding = 'Warren G. Harding'
+Coolidge = 'Calvin Coolidge'
+Hoover = 'Herbert Hoover'
+Franklin_Roosevelt = 'Franklin D. Roosevelt'
+Truman = 'Harry S. Truman'
+Eisenhower = 'Dwight D. Eisenhower'
+Kennedy = 'John F. Kennedy'
+Lyndon_Johnson = 'Lyndon B. Johnson'
+Nixon = 'Richard M. Nixon'
+Ford = 'Gerald R. Ford'
+Carter = 'Jimmy Carter'
+Reagan = 'Ronald Reagan'
+Father_Bush = 'George Bush'
+Clinton = 'William J. Clinton'
+Son_Bush = 'George W. Bush'
+Obama = 'Barack Obama'
+Trump = 'Donald Trump'
+
+president_names = [Washington, Adams, Jefferson, Madison, Monroe, Quincy_Adams, Jackson, Buren,
+                   William_Harrison, Tyler, Polk, Taylor, Fillmore, Pierce, Buchanan, Lincoln,
+                   Andrew_Johnson, Grant, Hayes, Garfield, Arthur, Cleveland, Benjamin_Harrison,
+                   McKinley, Theodore_Roosevelt, Taft, Wilson, Harding, Coolidge, Hoover, Franklin_Roosevelt,
+                   Truman, Eisenhower, Kennedy, Lyndon_Johnson, Nixon, Ford, Carter, Reagan, Father_Bush,
+                   Clinton, Son_Bush, Obama, Trump]
